180b19cce8e9821c2f8fc196d300ba729abd038d
Cincomari <142551398+Cincomari@users.noreply.github.com>
2024-04-22 23:33:23 -0400

    Update setup.py
    code change 2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="py_ball",
     version="1.41",
-    author="Patrick McFarlane & Avyay Varadarajan",
+    author="Mari Hudson & Anyrhi McCray",
     author_email="patmcfarla@gmail.com",
     description="Python API wrapper for stats.nba.com with a focus on \
                  NBA and WNBA applications",
